8a9bba186ba3d9d7b14dccab4e6fbf226c420a30
Aman-Jamshed <amanjamshed3@gmail.com>
2023-03-08 19:49:07 +0530

    solved isort issue

--- examples/models/colors.py
+++ examples/models/colors.py
@@ -16,9 +16,9 @@ from math import pi
 
 import pandas as pd
 
-from bokeh.colors import groups
 from bokeh.document import Document
 from bokeh.embed import file_html
+from bokeh.colors import groups
 from bokeh.models import (
     CategoricalAxis,
     CategoricalScale,
